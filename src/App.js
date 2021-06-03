3abe39f028f315c29e7172d394056e5808bc5b39
Syransong <ksysong@gmail.com>
2021-06-03 16:09:17 -0700

    refactor code to display json data nicely

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ function App({ login }) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(`http://api.github.com/users/${login}`)
-      .then((res) => res.json())
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
       .then(setData)
   }, []);
 
-  if(data) {
-    return <div>{JSON.stringify(data)}</div>
-  }
-  return (
-    <div>No User Available</div>
-  )
+  if (data) {
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url}/>
+      </div>
+    );
+  };
+
+  return <div>No User Available</div>
 }
 
 export default App;
