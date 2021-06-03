76f057c07b8d5f0100206edb6265ac95bef27cd1
Syransong <ksysong@gmail.com>
2021-06-03 15:11:13 -0700

    test useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
 function App(props) {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
 
   return (
     <>
-      <h1>Current emotion is {emotion}</h1>
+      <h1>Current emotion is {emotion} and {secondary}</h1>
       <button onClick={() => setEmotion("Frustrated")}>Frustrate</button>
+      <button onClick={() => setSecondary("Crabby")}>Make Crabby</button>
     </>
   )
 }
