a2d62efa9c54d4ec5b5deee320c41af4655c309c
Syransong <ksysong@gmail.com>
2021-06-03 15:58:49 -0700

    fetch github api using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-// import {useState, useEffect} from "react";
-import {useReducer} from "react";
+import {useState, useEffect} from "react";
+// import {useReducer} from "react";
 
 
-function App(props) {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked, // first arg is the function
-    false // 2nd arg is the initial state
-  );
+function App({ login }) {
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    fetch(`http://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+  }, []);
+
+  if(data) {
+    return <div>{JSON.stringify(data)}</div>
+  }
   return (
-    <>
-      <input 
-        type="checkbox"
-        value={checked} 
-        onChange={toggle} 
-      />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </>
+    <div>No User Available</div>
   )
 }
 
