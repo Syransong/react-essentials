d7238049cd3b36c40baa03c78d9c5263e15168b9
Syransong <ksysong@gmail.com>
2021-06-02 21:16:00 -0700

    create first h1 element

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement(
+    "h1", 
+    {style: { color: "blue"} }, 
+    "Hello!"
+  ),
   document.getElementById('root')
 );
 
