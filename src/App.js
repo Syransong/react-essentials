c28683afc4a4209fb15c856e6695b94fd15a9ae2
Syransong <ksysong@gmail.com>
2021-06-03 15:20:02 -0700

    test useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
+import {useReducer} from "react";
 
 
 function App(props) {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
+  const [checked, toggle] = useReducer(
+    (checked) => !checked, // first arg is the function
+    false // 2nd arg is the initial state
+  );
 
   return (
     <>
-      <h1>Current emotion is {emotion} and {secondary}</h1>
-      <button onClick={() => setEmotion("Frustrated")}>Frustrate</button>
-      <button onClick={() => setSecondary("Crabby")}>Make Crabby</button>
+      <input 
+        type="checkbox"
+        value={checked} 
+        onChange={toggle} 
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   )
 }
