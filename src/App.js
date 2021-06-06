8fe557bcfce3a52d054766343449755e1a6403e8
Syransong <ksysong@gmail.com>
2021-06-05 17:52:39 -0700

    create test using react testing library

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 // import {useReducer} from "react";
 
 
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, SetError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error)
-    return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
-
+function App() {
   return (
     <div>
-      <h1>{data.name}</h1>
-      <p>{data.location}</p>
-      <img alt={data.login} src={data.avatar_url}/>
+      <h1>Hello React Testing Library</h1>
     </div>
   );
 
